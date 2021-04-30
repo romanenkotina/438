45b6cefdf86e435123cf61ecfe611d6b5e24dabd
romanenko tina <spidersdonut@gmail.com>
2021-04-30 14:32:46 +0300

    Updated 26.04.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let userName = "tina";
+let str = "Forum 'My lil ponies'"
+
+let num = 100;
+let numFloat = 10.25;
+
+let bool = true;
+
+let x = undefined;
+let y = null;
+
+let obj = {};
+let mas = [];
+let func = () => { console.log("Function") };
+
+
+console.log(typeof bool);
+
+// 0b двоичная 
+// 0x 16 hex
+// 0o 8
+
+// starts with a ${userName[userName.length - 1]};
+// userName[0].toUpperCase() + userName.substring(1).toLowerCase();
+
+prompt("Type number: ", "100");
+
