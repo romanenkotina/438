2f706acd5ffe5b4de8eaca0b22f60e456baf3573
romanenko tina <spidersdonut@gmail.com>
2021-05-09 20:27:57 +0300

    Exercise 1

--- js/script.js
+++ js/script.js
@@ -1,20 +1,50 @@
-let userName = "tina";
-let str = "Forum 'My lil ponies'"
+// ******* 1 задача
+// Программа хранит в двух переменных курс доллара и евро. В первой переменной у вас хранится стоимость одного евро в рублях, во второй - стоимость одного доллара в рублях. Вы спрашиваете у пользователя, сколько рублей он хочет сконвертировать, получаете это число и считаете. Результат надо вывести на страницу с помощью alert.
 
-let num = 100;
-let numFloat = 10.25;
+let rateEuro = 89.64;
+let rateDollar = 73.7;
+let sumEuro, sumDollars;
 
-let bool = true;
+let rubles = prompt("Сумма в рублях:");
 
-let x = undefined;
-let y = null;
+if(!rubles && typeof rubles == "object") {
+  alert("Вы отменили!!!");
+}
+else {
+  if(rubles == "") {
+    alert("Вы ничего не ввели!!!");
+  }
+  else {
+    if(isNaN(+rubles)) {
+      alert("Это не число!");
+    }
+    else {
+      sumDollars = (rubles/rateDollar).toFixed(2);
+      sumEuro = (rubles/rateEuro).toFixed(2);
 
-let obj = {};
-let mas = [];
-let func = () => { console.log("Function") };
+      alert(`Сумма в долларах = ${sumDollars}$, сумма в евро = ${sumEuro}\u20AC.`);
+    }
+  }
+}
+
+// ******* Записи с первого дня
+// let userName = "tina";
+// let str = "Forum 'My lil ponies'"
+
+// let num = 100;
+// let numFloat = 10.25;
+
+// let bool = true;
+
+// let x = undefined;
+// let y = null;
+
+// let obj = {};
+// let mas = [];
+// let func = () => { console.log("Function") };
 
 
-console.log(typeof bool);
+// console.log(typeof bool);
 
 // 0b двоичная 
 // 0x 16 hex
@@ -23,5 +53,5 @@ console.log(typeof bool);
 // starts with a ${userName[userName.length - 1]};
 // userName[0].toUpperCase() + userName.substring(1).toLowerCase();
 
-prompt("Type number: ", "100");
+// prompt("Type number: ", "100");
 
