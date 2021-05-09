a20d208c4d0c9e68b508a187297c5ddc93df4c46
romanenko tina <spidersdonut@gmail.com>
2021-05-10 00:56:05 +0300

    Added mini calc

--- js/script.js
+++ js/script.js
@@ -1,3 +1,89 @@
+// //**** Мини-калькулятор ****
+// // сложение 2-х чисел
+// // разность 2-х чисел
+// // произведение 2-х чисел
+// // частное 2-х чисел
+// // возведение числа в n-степень
+// // корень n-й степени
+
+// let cancel = true;
+// let menu = `Ваш выбор (0-6):
+// 1. Сложение 2-х чисел
+// 2. Разность 2-х чисел
+// 3. Произведение 2-х чисел
+// 4. Деление 2-х чисел
+// 5. Возведение числа в n-ю степень
+// 6. Вычисление корня n-й степени
+// 0. Отмена`;
+
+// let num1, num2;
+// let result; //сумма, разность, произведение...
+
+// do {
+//   let operation = prompt(menu, "0");
+
+//   switch(operation) {
+//     case "1": num1 = +prompt("Первое число:");
+//               num2 = +prompt("Второе число:");
+
+//               result = num1 + num2;
+//               alert(`Сумма чисел = ${result}`);
+//               break;
+//     case "2": num1 = prompt("Первое число:");
+//               num2 = prompt("Второе число:");
+
+//               result = num1 - num2;
+//               alert(`Разность чисел = ${result}`);
+//               break;
+//     case "3": num1 = prompt("Первое число:");
+//               num2 = prompt("Второе число:");
+
+//               result = num1 * num2;
+//               alert(`Произведение чисел = ${result}`);
+//               break;
+//     case "4": num1 = prompt("Первое число:");
+//               num2 = prompt("Второе число:");
+
+//               if(num2 != 0) {
+//                 result = num1 / num2;
+//                 alert(`Частное 2-х чисел = ${result.toFixed(2)}`);
+//               }
+//               else {
+//                 alert(`Делить на 0 нельзя!`);
+//               }
+//               break;
+//     case "5": num1 = prompt("Число:");
+//               num2 = prompt("Степень:");
+
+//               if(num2 < 0) {
+//                 result = Math.pow(num1, num2).toFixed(4);  
+//               }
+//               else {
+//                 // result = num1**num2;
+//                 result = Math.pow(num1, num2);
+//               }
+
+//               alert(`Число ${num1} в степени ${num2} = ${result}`);
+//               break;
+//     case "6": num1 = prompt("Число:");
+//               num2 = prompt("Степень корня:");
+
+//               result = Math.pow(num1, 1 / num2);
+//               alert(`Корень степени ${num2} из числа ${num1} = ${result}`);
+
+//               break;
+    
+//     //отмена (закрытие меню)
+//     case "0": 
+//     case null: cancel = false;
+//               break;
+//     default:  alert("Ошибка! Неверный выбор!!!");
+//               break;
+//   }
+// } while(cancel);
+
+
+
 // ******** 5 задача
 // Выведите столбец чисел от 1 до 50.
 
