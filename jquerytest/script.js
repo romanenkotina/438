c09fa363cb128c49505d4fa2c912351d29527d8c
romanenko tina <spidersdonut@gmail.com>
2021-05-22 03:28:37 +0300

    Changes in jQuery test

--- jquerytest/script.js
+++ jquerytest/script.js
@@ -1,56 +1,67 @@
-// добавить затемнение фона
-// добавить клик вне окошка для закрытия
 // решить проблему с двойным кликом, когда адулт тру
-// создать куки
-// если сделать куки, поменять открытие страниц в одной вкладке
 // если сделать куки и он работает, как я думаю, найти способ стирать этот куки
+// перенаправить разноязычные страницы на разных уточек
 
-let adult = false; // статус пользователя: несовершеннолетний
+// let adult = false; // статус пользователя: несовершеннолетний
 
-// пользователь нажал на раздел 18+
-$("#agecheck").on("click", () => {
-  // проверяет статус пользователя
-  if (!adult) {
-    // если несовершеннолетний, открывает модальное окно
-    $(".popup").fadeIn("slow");
-    // пользователь нажал на кнопку перейти
-    // переводит пользователя в раздел и закрывает модальное окно
-    $(".goto").on("click", () => {
-      window.open("adult.html");
-      $(".popup").fadeOut("slow");
-      // меняет статус пользователя на совершеннолетнего
-      adult = true;
-      alert(
-        "Замечательно! Мы запомним это и в дальнейшем будем пускать вас в раздел сразу!"
-      );
-    });
-  } else {
-    // если совершеннолетний, переходит сразу в раздел 18+
-    // только почему-то приходится дважды кликать
-    $("#agecheck").on("click", () => {
-      window.open("adult.html");
-    });
+$(document).ready(function ($) {
+  // пользователь нажал на раздел 18+
+  $("#agecheck").on("click", () => {
+    // проверяет статус пользователя
+    // if (!adult) {
+    if (getCookie("over-18") == "true") {
+      // если несовершеннолетний, открывает модальное окно
+      $(".popup-fade").fadeIn("slow");
+      // пользователь нажал на кнопку перейти
+      // переводит пользователя в раздел и закрывает модальное окно
+      $(".goto").on("click", () => {
+        window.location.href = "adult.html";
+        $(".popup-fade").fadeOut("slow");
+        // меняет статус пользователя на совершеннолетнего
+        // adult = true;
+        document.cookie = "over-18=true";
+      });
+    } else {
+      // если совершеннолетний, переходит сразу в раздел 18+
+      // только почему-то приходится дважды кликать
+      $("#agecheck").on("click", () => {
+        window.location.href = "adult.html";
+      });
+    }
+  });
+
+  function getCookie(name) {
+    let cookies = document.cookie || "";
+    let cookie =
+      cookies.split("; ").find((row) => row.startsWith(name + "=")) || "";
+    return cookie.split("=")[1];
   }
-});
 
-// пользователь нажимает Закрыть
-// закрывает модальное окно
-$(".close").on("click", () => {
-  $(".popup").fadeOut("slow");
-});
+  // пользователь нажимает Закрыть
+  // закрывает модальное окно
+  $(".close").on("click", () => {
+    $(".popup-fade").fadeOut("slow");
+  });
+  // закрывает окно при клике вне окна
+  $(".popup-fade").click(function (e) {
+    if ($(e.target).closest(".popup").length == 0) {
+      $(this).fadeOut();
+    }
+  });
 
-// считает, исполнилось ли пользователю 18
-$("#bdayinput, .popup").on("change", () => {
-  const now = Date.now();
-  const bday = document.getElementById("bdayinput").value;
-  const diff = Math.abs(now - Date.parse(bday));
-  const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
-  // скрывает кнопку, пока возраст пользователя меньше 18
-  if (age >= 18) {
-    $(".goto").prop("disabled", false);
-  } else {
-    $(".goto").prop("disabled", true);
-  }
+  // считает, исполнилось ли пользователю 18
+  $("#bdayinput, .popup").on("change", () => {
+    const now = Date.now();
+    const bday = document.getElementById("bdayinput").value;
+    const diff = Math.abs(now - Date.parse(bday));
+    const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+    // скрывает кнопку, пока возраст пользователя меньше 18
+    if (age >= 18) {
+      $(".goto").prop("disabled", false);
+    } else {
+      $(".goto").prop("disabled", true);
+    }
+  });
 });
 
 // ограничение календаря до текущего дня (начало)
